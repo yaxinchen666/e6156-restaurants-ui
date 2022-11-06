9f10429c6e9ae44e1b7a759051fa927948f84e2e
ziwei_han <han@spectralmd.com>
2022-11-06 14:20:19 -0500

    api finished

--- src/components/Restaurants.js
+++ src/components/Restaurants.js
@@ -6,52 +6,52 @@ import Pagination from './Pagination';
 
 const Restaurants = () => {
     const [restaurants, setRestaurants] = useState([
-      {
-        'rest_id': 1,
-        'rest_name': 'Tuscany Steakhouse',
-        'rest_location': '117 W 58th St, New York, NY 10019',
-        'rest_size': 30
-      },
-      {
-        'rest_id': 2,
-        'rest_name': 'Gattopardo',
-        'rest_location': '13-15 W 54th St, New York, NY 10019',
-        'rest_size': 25
-      },
-      {
-        'rest_id': 3,
-        'rest_name': 'Carnegie Diner & Cafe',
-        'rest_location':  '205 W 57th St, New York, NY 10019',
-        'rest_size': 20
-      },
-      {
-        'rest_id': 4,
-        'rest_name': 'Mulligan\'s Pub',
-        'rest_location': '159 1st St, Hoboken, NJ 07030',
-        'rest_size': 30
-      },
-      {
-        'rest_id': 5,
-        'rest_name': 'Mills Tavern',
-        'rest_location': '125 Washington St, Hoboken, NJ 07030',
-        'rest_size': 25
-      },
+      // {
+      //   'rest_id': 1,
+      //   'rest_name': 'Tuscany Steakhouse',
+      //   'rest_location': '117 W 58th St, New York, NY 10019',
+      //   'rest_size': 30
+      // },
+      // {
+      //   'rest_id': 2,
+      //   'rest_name': 'Gattopardo',
+      //   'rest_location': '13-15 W 54th St, New York, NY 10019',
+      //   'rest_size': 25
+      // },
+      // {
+      //   'rest_id': 3,
+      //   'rest_name': 'Carnegie Diner & Cafe',
+      //   'rest_location':  '205 W 57th St, New York, NY 10019',
+      //   'rest_size': 20
+      // },
+      // {
+      //   'rest_id': 4,
+      //   'rest_name': 'Mulligan\'s Pub',
+      //   'rest_location': '159 1st St, Hoboken, NJ 07030',
+      //   'rest_size': 30
+      // },
+      // {
+      //   'rest_id': 5,
+      //   'rest_name': 'Mills Tavern',
+      //   'rest_location': '125 Washington St, Hoboken, NJ 07030',
+      //   'rest_size': 25
+      // },
     ])
 
-  // useEffect(() => {
-  //   axios.get("getAllResults")
-  //   .then(
-  //     res => {
-  //       console.log(res)
-  //       setRestaurants(res)
-  //     }
-  //   )
-  //   .catch(
-  //     error => {
-  //       console.log(error)
-  //     }
-  //   )
-  // }, []) 
+  useEffect(() => {
+    axios.get("https://e3pejg5go6.execute-api.us-east-1.amazonaws.com/restaurants")
+    .then(
+      res => {
+        console.log(res)
+        setRestaurants(res['data'])
+      }
+    )
+    .catch(
+      error => {
+        console.log(error)
+      }
+    )
+  }, []) 
 
   if (restaurants.length === 0) {
     return <>Still loading...</>;
